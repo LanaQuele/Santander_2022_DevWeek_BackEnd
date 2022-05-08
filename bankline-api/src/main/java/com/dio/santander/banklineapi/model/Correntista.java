9d1d205e1e5ce1c251441d227b5e3cb40268721f
LanaQuele <lanaquele@gmail.com>
2022-05-08 12:47:49 -0300

    mapeando as entidades com JPA

--- bankline-api/src/main/java/com/dio/santander/banklineapi/model/Correntista.java
+++ bankline-api/src/main/java/com/dio/santander/banklineapi/model/Correntista.java
@@ -1,9 +1,29 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tab_correntista" )
+
+
 public class Correntista {
+@Column(length = 60)	
 private String nome;
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer id;
-private String cpg;
+
+@Column(length = 20)
+private String cpf;
+
+@Embedded
 private Conta conta;
 public String getNome() {
 	return nome;
@@ -18,10 +38,10 @@ public void setId(Integer id) {
 	this.id = id;
 }
 public String getCpg() {
-	return cpg;
+	return cpf;
 }
-public void setCpg(String cpg) {
-	this.cpg = cpg;
+public void setCpg(String cpf) {
+	this.cpf = cpf;
 }
 public Conta getConta() {
 	return conta;
